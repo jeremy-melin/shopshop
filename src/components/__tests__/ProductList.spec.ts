dd4f2fb6af3b3a50e32a2c732f6a900151cfd9f2
Jérémy M <melin.jrm@gmail.com>
2024-07-17 17:58:11 +0200

    fix: components unit tests with pinia set up

--- src/components/__tests__/ProductList.spec.ts
+++ src/components/__tests__/ProductList.spec.ts
@@ -1,12 +1,17 @@
-import { describe, expect, it } from 'vitest';
-import { mount, shallowMount } from '@vue/test-utils';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { mount } from '@vue/test-utils';
 import ProductList from '../product/ProductList.vue';
 import { productsList } from '../../sampleProducts';
+import { createPinia, setActivePinia } from 'pinia';
 
 
 describe('ProductList', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia());
+    })
+      
     it('should display a list of given products', () => {
-        const wrapper = mount(ProductList, { propsData: { products: productsList } })
+        const wrapper = mount(ProductList, { props: { products: productsList } })
         expect(wrapper.html()).toContain(productsList[0].name);
     });
 })
--- src/components/__tests__/ShoppingCart.spec.ts
+++ src/components/__tests__/ShoppingCart.spec.ts
@@ -1,10 +1,15 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { mount } from "@vue/test-utils";
 import ShoppingCart from "../cart/ShoppingCart.vue";
+import { createPinia, setActivePinia } from "pinia";
 
 describe('ShoppingCart', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia());
+    });
+    
     it('should mount', () => {
         const wrapper = mount(ShoppingCart);
-        expect(wrapper.html()).toContain("SHOPPING CART");
+        expect(wrapper.html()).toBeDefined();
     });
 })
